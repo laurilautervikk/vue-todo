22fffb4739848e810b9e731c1c3e16fe486c5325
Lauri <lauri.lauter@vikk.ee>
2021-11-18 23:23:44 +0200

    Delete working

--- server/router.js
+++ server/router.js
@@ -9,19 +9,36 @@ const { Todos } = require("./dbConnection");
   res.send({ result });
 }); */
 
-// request === req ja response === res
+//Get all todos
 router.get("/get-todos", async function (request, response) {
   const result = await Todos.find();
   console.log(result);
   response.send(result);
 });
 
+//Get one todo
 router.get("/get-todo/:id", async function (request, response) {
   const result = await Todos.findOne({ _id: request.params.id });
   console.log(result);
   response.send(result);
 });
 
+//Change one todo
+router.patch("/patch-todo/:id", async function (request, response) { //NOT WORKING
+  const result = await Todos.updateOne({ _id: request.params.id },{$set: {STATUS: req.body.STATUS}});
+  console.log("/patch-todo/:id ",result);//NOT WORKING
+  response.send("/patch-todo/:id ",result);//NOT WORKING
+});
+
+//Delete one todo
+router.post("/delete-todo/:id", async function (request, response) {
+  const result = await Todos.deleteOne({ _id: request.params.id });
+  console.log(result);
+  response.send(result);
+});
+
+
+//Add a new todo
 router.post("/add-todo", async function (request, response) {
   if (request.body.title) {
     await Todos.create(request.body);
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,11 +12,12 @@
             @click="getTodo(todo._id)"
           >
           <p>{{ todo.title }}</p>
-          <p>{{ todo.status }}</p>
-          <button @click="completeTodo" type="submit" class="btn btn-primary m-1">
+          <p>{{ todo.status }}</p><!-- MUUDA STATUS > 'ACTIVE: true'
+          sealt edasi areta IF ELSE ja muuda kujundust vms -->
+          <button @click="completeTodo(todo._id)" type="submit" class="btn btn-primary m-1">
           It's Done
         </button>
-        <button @click="deleteTodo" type="submit" class="btn btn-primary m-1">
+        <button @click="deleteTodo(todo._id)" type="submit" class="btn btn-primary m-1">
           Delete
         </button>
           </li>
@@ -53,24 +54,45 @@ export default {
   props: {
     title: String,
   },
+
   setup() {
     const newTodo = ref("");
 
     const todosFromServer = ref([]);
     const singleTodo = ref({});
 
+    //Get all todos
     async function getTodos() {
       const result = await axios.get("/api/get-todos");
       todosFromServer.value = result.data;
       console.log(result.data);
     }
 
+    //Get one todo
     async function getTodo(id) {
       const result = await axios.get("/api/get-todo/" + id);
       singleTodo.value = result.data;
       console.log(result.data);
     }
 
+    //Change one todo
+    async function completeTodo(id) {//NOT WORKING
+      await axios.post(`/api/patch-todo/" + ${id}`, {//NOT WORKING
+        title: 'whatever',//NOT WORKING
+        status: "DONE",//NOT WORKING
+      });
+      console.log('sent a patch something')
+    }
+
+    //Delete one todo
+    async function deleteTodo(id) {
+      const result = await axios.post("/api/delete-todo/" + id);
+      singleTodo.value = result.data;
+      console.log(result.data);
+      await getTodos();
+    }
+
+    //Add a new todo
     async function addTodo() {
       await axios.post("/api/add-todo", {
         title: newTodo.value,
@@ -91,7 +113,15 @@ export default {
       singleTodo,
       getTodo,
       getTodos,
+      completeTodo,
+      deleteTodo,
     };
   },
 };
 </script>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
