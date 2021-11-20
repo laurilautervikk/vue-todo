1b5c55ffbc9d6e4ff03c7c291edd5dc0e545775b
Lauri <lauri.lauter@vikk.ee>
2021-11-20 14:11:01 +0200

    Password removed

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
 
-// Connecting to database without local DNS correction
-/* mongoose.connect(
-  "mongodb://hapukapsas:hapukapsas@cluster1-shard-00-00.ckjul.mongodb.net:27017,cluster1-shard-00-01.ckjul.mongodb.net:27017,cluster1-shard-00-02.ckjul.mongodb.net:27017/todoapp?ssl=true&replicaSet=atlas-13b4s0-shard-0&authSource=admin&retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-); */
-
+// Connecting to database
 mongoose.connect(
-  "mongodb+srv://hapukapsas:hapukapsas@cluster1.ckjul.mongodb.net/todoapp?retryWrites=true&w=majority",
+  "mongodb+srv://hapukapsas:<password>@cluster1.ckjul.mongodb.net/todoapp?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 const Schema = mongoose.Schema;
